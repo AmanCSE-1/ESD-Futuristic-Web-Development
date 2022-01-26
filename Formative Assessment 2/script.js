c8ef86c6dbb051c0244e57149af8618e15381c48
AmanCSE-1 <78953880+AmanCSE-1@users.noreply.github.com>
2022-01-26 18:01:34 +0530

    Create script.js

---
+++ Formative Assessment 2/script.js
@@ -0,0 +1,105 @@
+//const api_url = "<heroku_app_url>"
+const api_url = "https://formative-assessment-2.herokuapp.com/user";
+
+function loadData(records = []) {
+  var table_data = "";
+  for (let i = 0; i < records.length; i++) {
+    table_data += `<tr>`;
+    table_data += `<td>${records[i].name}</td>`;
+    table_data += `<td>${records[i].roll}</td>`;
+    table_data += `<td>${records[i].class}</td>`;
+    table_data += `<td>${records[i].email}</td>`;
+    table_data += `<td>`;
+    table_data += `<a href="edit.html?id=${records[i]._id}"><button class="btn btn-warning">Edit</button></a>`;
+    table_data += "&nbsp;&nbsp;";
+    table_data += `<button class="btn btn-danger" onclick=deleteData('${records[i]._id}')>Delete</button>`;
+    table_data += `</td>`;
+    table_data += `</tr>`;
+  }
+  //console.log(table_data);
+  document.getElementById("tbody").innerHTML = table_data;
+}
+
+function getData() {
+  fetch(api_url)
+    .then((response) => response.json())
+    .then((data) => {
+      console.table(data);
+      loadData(data);
+    });
+}
+
+function getDataById(id) {
+  fetch(`${api_url}/${id}`)
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      document.getElementById("id").value = data._id;
+      document.getElementById("name").value = data.name;
+      document.getElementById("roll").value = data.roll;
+      document.getElementById("class").value = data.class;
+      document.getElementById("email").value = data.email;
+    });
+}
+
+function postData() {
+  var name = document.getElementById("name").value;
+  var roll = document.getElementById("roll").value;
+  var class1 = document.getElementById("class").value;
+  var email = document.getElementById("email").value;
+  data = { name: name, roll: roll, class: class1, email: email};
+  fetch(api_url, {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      window.location.href = "index.html";
+    });
+}
+
+function putData() {
+  var _id = document.getElementById("id").value;
+  var name = document.getElementById("name").value;
+  var roll = document.getElementById("roll").value;
+  var class1 = document.getElementById("class").value;
+  var email = document.getElementById("email").value;
+  data = { _id: _id, name: name, roll: roll, class: class1, email: email};
+  fetch(api_url, {
+    method: "PUT",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      console.table(data);
+      window.location.href = "index.html";
+    });
+}
+
+function deleteData(id) {
+  user_input = confirm("Are you sure you want to delete this record?");
+  if (user_input) {
+    fetch(api_url, {
+      method: "DELETE",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ _id: id }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        window.location.reload();
+      });
+  }
+}
